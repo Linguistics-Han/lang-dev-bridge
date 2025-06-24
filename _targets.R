1ec77ecc7b87bb1c44ccce6edd6c75941a469fe1
Liang Zhang <psychelzh@outlook.com>
2025-06-24 11:25:27 +0800

    Use file to store variable names

--- _targets.R
+++ _targets.R
@@ -6,6 +6,11 @@ tar_option_set(
 )
 
 list(
+  tarchetypes::tar_file_read(
+    vars_config,
+    "data/groups.csv",
+    read = read_csv(!!.x, show_col_types = FALSE)
+  ),
   tar_target(file_data, "data/written.xlsx"),
   tar_target(data, load_all_sheets(file_data)),
   tar_target(data_clean, clean_data(data)),
@@ -13,7 +18,7 @@ list(
     avg_perf,
     data_clean |>
       summarise(
-        across(T长度:type短语结构类型, mean),
+        across(all_of(vars_config$variable), mean),
         .by = c(time, SID)
       )
   ),
